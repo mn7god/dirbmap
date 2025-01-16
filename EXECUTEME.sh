482517c99d39baab4fe22f7f8852a4918e77bcb0
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2025-01-16 10:54:28 -0300

    Create EXECUTEME.sh

---
+++ EXECUTEME.sh
@@ -0,0 +1,14 @@
+python3 -m venv py
+py/bin/pip3 install requests
+pip3 install requests --break-system-packages
+sudo apt install -y smbclient
+sudo apt install -y gvncviewer
+sudo apt install -y snmp
+sudo apt install -y freerdp2-x11
+sudo apt install -y postgresql-client-common
+sudo apt install -y mariadb-client-core
+sudo apt install -y mysql-client-core-8.0
+sudo apt install -y ftp-ssl
+sudo apt install -y tnftp
+sudo apt install -y gftp-txt
+sudo apt install -y inetutils-ftp
